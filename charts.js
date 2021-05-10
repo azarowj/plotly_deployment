209f21587fdef824af5786c4eab2d966475d4656
Jennifer Azarow <JAzarow@azarowj.lan>
2021-05-09 20:45:58 -0400

    working through challenge

--- charts.js
+++ charts.js
@@ -68,15 +68,13 @@ function buildCharts(sample) {
     var metaArray = metaD.filter(sampleObj => sampleObj.id == sample);
 
     //  5. Create a variable that holds the first sample in the array.
-    var firstinArray = firstDesiredArray[0];
+    var firstinArray = filterDesiredArray[0];
 
     // 2. Create a variable that holds the first sample in the metadata array.
     var metaFirst = metaArray[0];
 
-    // Create variables that hold the otu_ids, otu_labels, and sample_values.
-
-
     // 3. Create a variable that holds the washing frequency.
+    var washFreq = metaFirst.wfreq;
 
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
@@ -137,12 +135,30 @@ function buildCharts(sample) {
     
     
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [
+    var gaugeData = [{
+      value: washFreq,
+      type: 'indicator',
+      mode: 'gauge+number',
+      title: {text: '<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week'},
+      gauge: {
+        axis: {range: [null, 10], dtick: '2'},
+        bar:{ color: 'black'},
+        steps: [
+          {range: [0, 2], color: 'red'},
+          {range: [2, 4], color: 'organge'},
+          {range: [4, 6], color: 'yellow'},
+          {range: [6, 8], color: 'green'},
+          {range: [8, 10], color: 'blue'}
+
+        ],
+        dtick: 2
+      }
      
-    ];
+    }];
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
+      automargin: true
      
     };
 
